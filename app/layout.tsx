79a88834e28cd79c03711bffc443d79be8d7c208
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-09 12:23:33 -0500

    feat: include inter and space grotesk fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
 
 import "./globals.css";
 import type { Metadata } from "next";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 export const metadata: Metadata = {
   title: "Dev Overflow",
   description: "Stack Overflow clone for Developers",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +32,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
