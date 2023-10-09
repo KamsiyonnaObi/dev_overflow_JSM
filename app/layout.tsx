73fea9b7d25a7971e60df84bc3cf7aadb8330225
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2023-10-09 11:32:14 -0500

    chore: remove font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,6 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dev Overflow",
@@ -20,7 +17,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
